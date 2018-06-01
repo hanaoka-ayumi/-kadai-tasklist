4946817523c94faa4e0e4f23d3ea4d025de375f3
Ayumi Hanaoka <ayumi.a.hanaoka@rakuten.com>
2018-06-01 07:25:23 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,20 +2,26 @@
 
 @section('content')
 
-<h1>タスク新規登録ページ</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+ <h1>タスク新規登録ページ</h1>
+ 
+  <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,16 +4,31 @@
 
 <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
 
-    <p>タスク: {{ $task->content }}</p>
-    <p>ステータス: {{ $task->status }}</p>
-    
-    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
+ <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $task->status }}</td>
+        </tr>
+    </table>
 
-    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+    
+   {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+
+   {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
+@endsection
 
 <!-- Write content for each page here -->
 
-@endsection
+
+
