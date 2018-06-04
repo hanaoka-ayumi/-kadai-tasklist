c977cc2df05641c45dfe6900916bbbab1e35f61f
Ayumi Hanaoka <ayumi.a.hanaoka@rakuten.com>
2018-06-04 01:07:30 +0000

    finally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,23 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+                
     </head>
 
     <body>
          @include('commons.navbar')
+         
         
         <div class="container">
          @include('commons.error_messages')
          @yield('content')
+         
+         <div class="row">
+             
+               
+            </div>
+            
          </div>
          
     </body>
